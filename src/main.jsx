58e2f3961310cedeb1464546f749afd154617ed6
Shahinul99 <rumaisaabrar99@gmail.com>
2025-09-14 11:41:30 -0400

    update all 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./components/Root/Root.jsx";
+import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
+import HomePage from "./components/HomePage/HomePage.jsx";
+import DashBoard from "./components/DashBoard/DashBoard.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children : [
+      {
+        path : '/',
+        element : <HomePage></HomePage>,
+      },
+      {
+        path : 'DashBoard',
+        errorElement : <DashBoard></DashBoard>
+      },
+    ]
   },
 ]);
 
